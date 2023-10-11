6dcbecc778f3a01867080b61dd3719708ce512fa
Addison Kline <klineaddison@gmail.com>
2023-10-11 15:34:11 -0400

    updating from laptop end

--- test.py
+++ test.py
@@ -1,24 +1,19 @@
-import random
+from pybaseball import schedule_and_record, team_batting
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-results = []
-
-for i in range(100):
-    escaped = False
-    days = 0
-
-    while (not escaped):
-        rand = random.random()
-
-        if (rand >= 0.5): # door 1
-            days += 2
-        elif (rand >= 0.2): # door 2
-            days += 4
-        else: # door 3
-            days += 1
-            escaped = True
-    
-    print(f'Iteration: {i+1}: {days} days')
-    results.append(days)
-
-print(f'Mean: {np.mean(results)}')
+teams = team_batting(2023)['Team'].values
+runs = np.array([])
+"""
+for team in teams:
+    time.sleep(2)
+    print(f'Currently on team: {team}')
+    schedule = schedule_and_record(2023, team)
+    runs = np.append(arr=runs, values=schedule['R'])
+"""
+schedule = schedule_and_record(2023, 'LAD')
+print(np.mean(schedule['R']))
+plt.hist(schedule['R'])
+plt.show()
